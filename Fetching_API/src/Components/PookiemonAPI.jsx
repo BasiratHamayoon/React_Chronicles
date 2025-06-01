4cf76ea1b9e6d0f2068df1d983be31a7c526090c
Basirat Hamayoon <basirathumayun@gmail.com>
2025-06-01 18:19:22 +0500

    create a loading state and error state

--- Fetching_API/src/Components/PookiemonAPI.jsx
+++ Fetching_API/src/Components/PookiemonAPI.jsx
@@ -1,23 +1,34 @@
 import { useState, useEffect } from "react";
 import "./PookiemonApi.css";
 export const PookiemoonApi = () => {
-    const [apiData, setApiData] = useState();
+    const [ apiData, setApiData ] = useState(null);
+    const [ loading, setLoading ] = useState(true);
+    const [ error, setError ] = useState()
     const url = "https://pokeapi.co/api/v2/pokemon/pikachu";
     const fetchData = () => {
         fetch(url)
         .then((res) => res.json())
         .then((data) => {
-            setApiData(data)
+            setApiData(data);
+            setLoading(false);
         })
-        .catch((error) => console.log(error));
+        .catch((error) => {
+            console.log(error);
+            setError(error.message);
+            setLoading(false)
+        });
     }
     useEffect(() => {
             fetchData();
         }, [])
         console.log(apiData)
-        if(!apiData) {
-            return <div>Loading...</div>
+        if(loading) {
+            return <h1 className="loading">Loading...</h1>
         }
+        if(error) {
+            return <h1>{error}</h1>
+        }
+
     return (
         <>
            <section className="container">
@@ -26,6 +37,11 @@ export const PookiemoonApi = () => {
                     <div className="card">
                             <img src={apiData.sprites.other.dream_world.front_default} alt={apiData.name} />
                             <h1>{apiData.name}</h1>
+                            <div className="info">
+                                <p>Weight: {apiData.weight}</p>
+                                <p>Height: {apiData.height}</p>
+                            </div>
+                            <p>Speed: {apiData.stats[5].base_stat}</p>
                     </div>
                 </div>
            </section>
